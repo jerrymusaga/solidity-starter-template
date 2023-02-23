81c2b9410a14b813c3938bc8186f244e15684804
jerrymusaga <musagatechnology@gmail.com>
2023-02-23 02:27:46 +0100

    accounts

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,5 +1,14 @@
-import { HardhatUserConfig } from "hardhat/config";
+import { HardhatUserConfig, task } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+
+task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+  const accounts = await hre.ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address);
+    console.log(await account.getBalance())
+  }
+});
 
 const config: HardhatUserConfig = {
   paths: { tests: "test" },
